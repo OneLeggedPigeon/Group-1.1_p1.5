e5e9de73e44d814ff43e75a2d391db68b18ec6ec
Nate Ford <nathaniel.ford@revature.net>
2021-03-29 16:41:09 -0500

    empty methods added to ConnectionUtil.java

--- src/main/java/com/revature/util/ConnectionUtil.java
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -1,5 +1,15 @@
 package com.revature.util;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 //Should by used by ReimbursementDao
 public class ConnectionUtil {
+    public static ConnectionUtil getInstance() {
+        return null;
+    }
+
+    public Connection getConnection() {
+        return null;
+    }
 }
