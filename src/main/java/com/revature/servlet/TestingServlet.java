fd624b32c51609744576899b9a950980113aad1a
Nate Ford <nathaniel.ford@revature.net>
2021-03-30 10:22:14 -0500

    switched servlet to use annotation

--- src/main/java/com/revature/servlet/TestingServlet.java
+++ src/main/java/com/revature/servlet/TestingServlet.java
@@ -6,14 +6,14 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 
-@WebServlet
+@WebServlet(urlPatterns = "/test")
 public class TestingServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-            throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        PrintWriter out = response.getWriter();
+        System.out.println(req.toString());
+
+        PrintWriter out = resp.getWriter();
         out.println("<HTML>");
         out.println("<HEAD>");
         out.println("<TITLE>Servlet Testing</TITLE>");
@@ -22,5 +22,13 @@ public class TestingServlet extends HttpServlet {
         out.println("Welcome to the Servlet Testing Center");
         out.println("</BODY>");
         out.println("</HTML>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String orderNumber = req.getParameter("testString");
+        System.out.println(orderNumber);
+        resp.getWriter().print(orderNumber);
     }
 }
