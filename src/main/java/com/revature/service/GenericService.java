32fa73f14375fe9ca2c7872556e5aaf50a94dde6
Nate Ford <nathaniel.ford@revature.net>
2021-03-30 18:24:20 -0500

    Added Service Interface

--- src/main/java/com/revature/service/GenericService.java
+++ src/main/java/com/revature/service/GenericService.java
@@ -3,11 +3,12 @@ package com.revature.service;
 import org.apache.log4j.Logger;
 
 import java.util.List;
+import java.util.Map;
 
 public interface GenericService<T> {
-    public void create(String...strings);
-    public void createOrUpdate(String...strings);
-    public void delete(String...strings);
+    public void create(Map<String,String> map);
+    public void createOrUpdate(Map<String,String> map);
+    public void delete(Map<String,String> map);
     public List<T> getList();
     public T getById(int id);
     public List<T> getByUserId(int id);
