7ee4e0502336dae442d45db599a71430c91b4488
Nate Ford <nathaniel.ford@revature.net>
2021-03-30 14:51:25 -0500

    Crud Operations are now called by the dispatcher

--- src/main/java/com/revature/controller/Dispatcher.java
+++ src/main/java/com/revature/controller/Dispatcher.java
@@ -1,7 +1,16 @@
 package com.revature.controller;
 
-import com.revature.dao.ReimbursementDao;
-import com.revature.dao.UserDao;
+import com.revature.dao.*;
+import com.revature.model.*;
+import com.revature.servlet.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
 
 //https://www.tutorialspoint.com/design_pattern/front_controller_pattern.htm
 public class Dispatcher {
@@ -13,12 +22,51 @@ public class Dispatcher {
         userView = new UserDao();
     }
 
-    public void dispatch(String request){
-        if(request.equalsIgnoreCase("USER")){
+    public void dispatch(Class<? extends HttpServlet> clazz, httpMethod method, HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        if(clazz.equals(UserServlet.class)){
             // do user thing
-        }
-        else{
+            switch(method){
+                case GET:
+                    if (req.getParameter("get-all") != null && req.getParameter("get-all").equals("true")){
+                        out.println("Users");
+                        for(User u : userView.getList()){
+                            out.println(u.toString());
+                        }
+                    } else {
+
+                    }
+                    break;
+                case PUT:
+                    break;
+                case POST:
+                    break;
+                case DELETE:
+                    break;
+                default:
+                    RuntimeException e = new RuntimeException("You shouldn't have come here");
+                    e.printStackTrace();
+                    throw e;
+            }
+        } else{
             // do something else
+        }
+    }
+
+    /**
+     *
+     * @param req HttpServletRequest
+     * @param param parameter name
+     * @param expected value to check <code>param</code> against
+     * @return true if <code>param</code> and <code>expected</code> match, false otherwise
+     * @throws ServletException if the parameter doesn't exist
+     */
+    private boolean checkParameter(HttpServletRequest req, String param, String expected) throws ServletException{
+        if (req.getParameter(param) == null){
+            throw new ServletException("Parameter doesn't exist", new NullPointerException());
+        } else {
+            return req.getParameter(param).equals(expected);
         }
     }
 }
--- src/main/java/com/revature/servlet/ReimbursementServlet.java
+++ src/main/java/com/revature/servlet/ReimbursementServlet.java
@@ -1,5 +1,7 @@
 package com.revature.servlet;
 
+import com.revature.controller.FrontController;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,42 +11,5 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/reimbursement")
-public class ReimbursementServlet extends HttpServlet {
-
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        PrintWriter out = resp.getWriter();
-        out.println("<HTML>");
-        out.println("<HEAD>");
-        out.println("<TITLE>Reimbursement</TITLE>");
-        out.println("</HEAD>");
-        out.println("<BODY>");
-        out.println("Reimbursement");
-        out.println("</BODY>");
-        out.println("</HTML>");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        System.out.println("POST: "+req.getParameter("testString"));
-
-        String orderNumber = req.getParameter("testString");
-        System.out.println(orderNumber);
-        resp.getWriter().print(orderNumber);
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        System.out.println("PUT: "+req.getParameter("testString"));
-
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        System.out.println("DELETE: "+req.getParameter("testString"));
-
-    }
+public class ReimbursementServlet extends CustomServlet {
 }
