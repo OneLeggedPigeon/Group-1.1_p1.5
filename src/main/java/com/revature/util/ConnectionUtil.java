bba270a7bbebf851c5dd95ebbcf85300f70b47c1
Nate Ford <nathaniel.ford@revature.net>
2021-03-31 10:57:54 -0500

    can now connect to the database with ConnectionUtil.getConnection()

--- src/main/java/com/revature/util/ConnectionUtil.java
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -1,15 +1,35 @@
 package com.revature.util;
 
+import com.revature.config.DatabaseProperties;
+
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 //Should by used by ReimbursementDao
 public class ConnectionUtil {
+    private static ConnectionUtil instance;
+
     public static ConnectionUtil getInstance() {
-        return null;
+        if(instance == null){
+            instance = new ConnectionUtil();
+        }
+        return instance;
     }
 
     public Connection getConnection() {
-        return null;
+        try {
+            DatabaseProperties props = DatabaseProperties.getInstance();
+
+            String connectionTemplate = props.getProfile();
+            String url = props.getPropertyByKey(connectionTemplate+".url")+"?currentSchema="+props.getSchema();
+            return DriverManager.getConnection(
+                    url,
+                    props.getPropertyByKey(connectionTemplate+".username"),
+                    props.getPropertyByKey(connectionTemplate+".password"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
